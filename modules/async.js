1e66cf4c7e9b85fa38ae5259f2ae46f4c68793e2
KaziNazmusSakib <kazinazmussakib36@gmail.com>
2026-05-22 11:13:54 +0600

    Update perfectly

---
+++ modules/async.js
@@ -0,0 +1,50 @@
+// function myFunction(callback) {
+// // Do Some work...
+// //call the callback function
+//   setTimeout(() => {
+//      dconstata = { name: "Sakib", age: "21"}
+//     callback(data);
+//   }, 3000);
+// }
+
+// myFunction((data) => {
+//   console.log("Data:", data);
+// });
+
+
+// function mydata() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const data = { name: "Rohit", age : 23 };
+//       resolve(data);
+//     }, 2000);
+//   });
+
+// }
+
+
+// mydata()
+// .then((data) => {
+//   console.log("Data:", data);
+// })
+// .catch((error) => {
+//   console.log("Error:", error);
+// });
+
+// function fetchData(callback) {
+//    return setTimeout((data) => {
+//        data = { name: "Sakib" , age: "31"};
+//       callback (null, data);
+//    }, 1000);
+// }
+
+// fetchData((error, data) => {
+//   if (error){
+//     console.log("Error: " + error);
+//   }
+//   else{
+//     console.log( "Name: " + data.name + " " + "Age: " + data.age);
+    
+//   }
+// });
+
--- modules/compare_array.js
+++ modules/compare_array.js
@@ -52,5 +52,58 @@
 
 // console.log(a1.every(isBelowThreshold));
 
+// const _= require("lodash");
+
+// let a1 = [1, 2, 3, 4, 5];
+
+// let a2 = [1, 2, 3, 4, 5];
+
+// console.log("The equal value is: " + _.isEqual(a1, a2));
+
+
+// Using Set
+
+// function compareArrays(a1, a2) {
+//     const s1 = new Set(a1);
+//     const s2 = new Set(a2);
+
+//     if (s1.size !== s2.size) {
+//       return false;
+//     }
+       
+    
+
+//     for(const item of s1) {
+//       if(!s2.has(item)) {
+//         return false;
+//       } 
+//     }
+    
+//     return true;
+//   }
+
+
+// const a1 = [1, 2, 3, 4];
+// const a2 = [4, 3, 2, 1];
+
+// console.log(compareArrays(a1, a2));
+
+
+// let a1 = [1, 2, 3, 4, 5];
+// let a2 = [5, 4, 3, 2, 1];
+// let a3 = [1, 2, 3, 4, 6];
+
+// console.log(arraysEqual())
+
+function myFunction(callback) {
+  setTimeout(() => {
+      const data = { name: "Aman", age: 21 };
+      callback(data);
+  }, 3000);
+}
+
+myFunction((data) => {
+    console.log("Data:", data);
+});
 
 
